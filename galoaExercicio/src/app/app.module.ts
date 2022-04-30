4cea69d0dbc7e5ebc0f2e7e0a44fd5f70bcd4bd8
joaopbg <joaopedrobarsaglini@gmail.com>
2022-04-30 19:12:26 -0300

    first section implemented

--- galoaExercicio/src/app/app.module.ts
+++ galoaExercicio/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContentPageComponent } from './content-page/content-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentPageComponent
   ],
   imports: [
     BrowserModule,
